36161f9420f7260148a4c200544ad3e44448be51
lucas <sacool@web.de>
2026-06-01 11:20:40 +0200

    implemented constructor for SensorReading

--- session01/Hallo.java
+++ session01/Hallo.java
@@ -1,4 +1,5 @@
 public class Hallo {
+
     public static void main(String[] args) {
         System.out.println("Hallo, Java!");
         int zahl = 42;
@@ -11,9 +12,11 @@ public class Hallo {
         String text = "Java ist " + buchstabe + " Plus!";
         System.out.println(text);
 
-        SensorReading r1 = new SensorReading(1, "2024-06-01T12:00:00Z", 19.3, 64.2);
-        System.out.println("Temperatur: " + r1.temperature);
-        SensorReading r2 = new SensorReading(1, "2024-06-01T12:05:00Z", -300.0, 64.2);
-        System.out.println("Temperatur: " + r2.temperature);
+        SensorReading r1 = new SensorReading(1, "S1", 19.3, 64.2);
+        r1.print();
+        SensorReading r2 = new SensorReading(2, null, -300.0, -78.0);
+        r2.print();
+        SensorReading r3 = new SensorReading(3, "S2", 25.0, 1200.3);
+        r3.print();
     }
 }
--- session01/SensorReading.java
+++ session01/SensorReading.java
@@ -1,18 +1,45 @@
 public class SensorReading {
-    public int sensorId;
-    public String timestamp;
-    public float temperature;
-    public float humidity;
 
-    public SensorReading(int sensorId, String timestamp, float temperature, float humidity) {
-        this.sensorId = sensorId;
-        this.timestamp = timestamp;
-        if (temperature < -273.15) {
-            System.err.println("Ungueltige Temperatur: " + temperature);
-            this.temperature = -273.15;
+    public int seq;
+
+    public String sensorId;
+
+    public double temperatureC;
+
+    public double humidityPct;
+
+    public SensorReading(int seq, String sensorId, double temperatureC, double humidityPct) {
+        this.seq = seq;
+
+        if (sensorId == null || sensorId == "") {
+            System.err.println("Ungueltige Sensor-ID: null or empty");
+            this.sensorId = "unknown";
         } else {
-            this.temperature = temperature;
+            this.sensorId = sensorId;
         }
-        this.humidity = humidity;
+
+        if (temperatureC < -273.15) {
+            System.err.println("Ungueltige Temperatur: " + temperatureC);
+            this.temperatureC = -273.15;
+        } else {
+            this.temperatureC = temperatureC;
+        }
+
+        if (humidityPct < 0.0) {
+            System.err.println("Ungueltige Luftfeuchtigkeit: " + humidityPct);
+            this.humidityPct = 0.0;
+        } else if (humidityPct > 100.0) {
+            System.err.println("Ungueltige Luftfeuchtigkeit: " + humidityPct);
+            this.humidityPct = 100.0;
+        } else {
+            this.humidityPct = humidityPct;
+        }
+    }
+
+    public void print() {
+        System.out.println("Seq: " + this.seq);
+        System.out.println("Sensor-ID: " + this.sensorId);
+        System.out.println("Temperatur: " + this.temperatureC);
+        System.out.println("Luftfeuchtigkeit: " + this.humidityPct);
     }
 }
